4b822531a71f55bb81b43ceb8b2c9ca2ecf4d732
shilpan2704 <101493306+shilpan2704@users.noreply.github.com>
2022-03-21 13:17:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
+pipeline {
+    agent any 
+    stages {
+        stage('Build ') { 
+            steps {
+              print("hello world");
+            }
+        }
+        stage('Test') { 
+            steps {
+                
+               print("Hi, this is shilpa from PES UNIVERSITY");
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo 'shilpa'
+            }
+        }
+    }
 
-print("hello world");
-print("Hi, this is shilpa from PES UNIVERSITY");
+
